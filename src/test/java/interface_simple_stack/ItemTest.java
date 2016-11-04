26e11d7cfae4011adfd00e50f149bc8ef70b446c
Yuuon <yuuon.himemiya@gmail.com>
2016-11-04 10:31:29 +0100

    #1

--- src/test/java/interface_simple_stack/ItemTest.java
+++ src/test/java/interface_simple_stack/ItemTest.java
@@ -1,10 +1,31 @@
 package interface_simple_stack;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Administrator on 2016/11/4.
  */
 public class ItemTest {
+    @Test
+    public void getNumero() throws Exception {
+
+    }
+
+    @Test
+    public void setNumero() throws Exception {
+
+    }
+
+    @Test
+    public void getNom() throws Exception {
+
+    }
+
+    @Test
+    public void setNom() throws Exception {
+
+    }
 
 }
